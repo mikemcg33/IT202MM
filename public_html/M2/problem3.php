1e260d5a9c07d6db388c867fb75d23ca58747d86
mikemcg33 <mkm@njit.edu>
2023-02-13 20:14:26 -0500

    Day two edits p4

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -9,7 +9,7 @@ function bePositive($arr) {
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive) 
     foreach($arr as $num){
-        echo var_dump(abs($num));
+        echo (gettype($num)),(abs($num));
     }
     //hint: may want to use var_dump() to show final data types
 }
