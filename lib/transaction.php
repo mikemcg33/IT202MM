33af54fefe6f9b89311c2e9a82836a35235f17bc
mikemcg33 <mkm@njit.edu>
2023-04-30 18:01:42 -0400

    Accounts edits

---
+++ lib/transaction.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Money should be passed as a positive value.
+ * $losing should be where the money is coming from
+ * $gaining should be where the money is going
+ */
+function transactions($money, $reason, $losing = -1, $gaining = -1, $details = "")
+{
+    //I'm choosing to ignore the record of 0 point transactions
+    if ($money > 0) {
+        $query = "INSERT INTO Money_History (src, dest, diff, reason, details) 
+            VALUES (:acs1, :acd1, :pc, :r,:m), 
+            (:acs2, :acd2, :pc2, :r, :m)";
+        //I'll insert both records at once, note the placeholders that are kept the same and the ones changed.
+        $params[":acs1"] = $losing;
+        $params[":acd1"] = $gaining;
+        $params[":r"] = $reason;
+        $params[":m"] = $details;
+        $params[":pc"] = ($money * -1);
+
+        $params[":acs2"] = $gaining;
+        $params[":acd2"] = $losing;
+        $params[":pc2"] = $money;
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute($params);
+            //Only refresh the balance of the user if the logged in user's account is part of the transfer
+            //this is needed so future features don't waste time/resources or potentially cause an error when a calculation
+            //occurs without a logged in user
+            refresh_account_balance($losing);
+            refresh_account_balance($gaining);
+            return true;
+        } catch (PDOException $e) {
+            error_log(var_export($e->errorInfo, true));
+            flash("There was an error transfering money", "danger");
+        }
+    }
+    return false;
+}
--- public_html/Project/create_account.php
+++ public_html/Project/create_account.php
@@ -3,33 +3,42 @@ require(__DIR__ . "/../../partials/nav.php");
 
 // Check if form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $db = getDB();
+  try {
+    $db = getDB();
   
-  // Get account type and deposit amount from form
-  $account_type = $_POST["account_type"];
-  $deposit_amount = $_POST["deposit_amount"];
+    // Get account type and deposit amount from form
+    $account_type = $_POST["account_type"];
+    $deposit_amount = $_POST["deposit_amount"];
   
-  // Insert new account record for user with null account number
-  $user_id = get_user_id();
+    // Insert new account record for user with null account number
+    $user_id = get_user_id();
 
-  if (!empty($user_id)) {
-  $stmt = $db->prepare("INSERT INTO Accounts (user_id, account_type, balance) VALUES (?, ?, ?)");
-  $stmt->execute([$user_id, $account_type, $deposit_amount]);
+    if (!empty($user_id)) {
+      $stmt = $db->prepare("INSERT INTO Accounts (user_id, account_type, balance) VALUES (?, ?, ?)");
+      $stmt->execute([$user_id, $account_type, $deposit_amount]);
+    }
+
+    // Get last insert id
+    $account_id = $db->lastInsertId();
+
+    // Generate account number using left-padding
+    $account_number = str_pad($account_id, 12, '0', STR_PAD_LEFT);
+
+    // Update account record with new account number
+    $stmt = $db->prepare("UPDATE Accounts SET account_number = ? WHERE id = ?");
+    $stmt->execute([$account_number, $account_id]);
+
+    //Flash success
+    flash("Welcome! Your account has been created successfully","success");
+    
+    // Redirect to account list page
+    header("Location: accounts.php");
+    exit();
+  } catch (PDOEXCEPTION $e) { 
+    //Flash fail
+    flash("an error occured while creating your account","danger");
+    error_log(var_export($e, true));
   }
-
-  // Get last insert id
-  $account_id = $db->lastInsertId();
-
-  // Generate account number using left-padding
-  $account_number = str_pad($account_id, 12, '0', STR_PAD_LEFT);
-
-  // Update account record with new account number
-  $stmt = $db->prepare("UPDATE Accounts SET account_number = ? WHERE id = ?");
-  $stmt->execute([$account_number, $account_id]);
-
-  // Redirect to account list page
-  header("Location: accounts.php");
-  exit();
 }
 ?>
 
